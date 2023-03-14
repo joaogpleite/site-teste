39c83d3a18d9b78c8b14acca618ff6a0268cd074
João Gabriel Leite <114266021+joaogpleite@users.noreply.github.com>
2023-03-13 22:09:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+menu = """
+<a href="/">Página Inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
+<br>
+"""
+
 @app.route("/")
 def index():
-  return "Olá, João! Esse é o seu primeiro site! Parabéns, cabeção!"
+  return menu + "Olá, João! Esse é o seu primeiro site! Parabéns, cabeção!"
 
 app.route("/sobre")
 def sobre():
-  return "João Gabriel Leite é jornalista e Redator".
+  return menu + "João Gabriel Leite é jornalista e Redator".
 
 app.route("/contato")
 def contato():
-  return "não tem, eu não quero ser contatado por ninguém."
+  return menu + "não tem, eu não quero ser contatado por ninguém."
