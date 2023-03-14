380b845df4e0e6be7e0f09c6a4ffd7020da28801
João Gabriel Leite <114266021+joaogpleite@users.noreply.github.com>
2023-03-13 21:51:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
   return "Olá, João! Esse é o seu primeiro site! Parabéns, cabeção!"
+
+app.route("/sobre")
+def sobre():
+  return "João Gabriel Leite é jornalista e Redator.
+
+app.route("/contato")
+def contato():
+  return "não tem, eu não quero ser contatado por ninguém."
